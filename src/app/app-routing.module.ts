18204b00df87aa3c76724bd05cb0bdc9a7169c78
Thierno DIALLO <thiernotd.td@gmail.com>
2023-02-25 10:16:45 +0100

    create module Pokemon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
-import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
 import { PageNoteFoundComponent } from './page-note-found/page-note-found.component';
 
 const routes: Routes = [
-  { path: 'pokemons',component: ListPokemonComponent},
-  { path: 'pokemon/:id',component: DetailPokemonComponent},
   { path: '', redirectTo: 'pokemons', pathMatch: 'full'}, 
   {path: '**', component: PageNoteFoundComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BorederCardDirective } from './boreder-card.directive';
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
-import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
-import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
 import { PageNoteFoundComponent } from './page-note-found/page-note-found.component';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BorederCardDirective,
-    PokemonTypeColorPipe,
-    ListPokemonComponent,
-    DetailPokemonComponent,
     PageNoteFoundComponent
   ],
   imports: [
     BrowserModule,
+    PokemonModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
